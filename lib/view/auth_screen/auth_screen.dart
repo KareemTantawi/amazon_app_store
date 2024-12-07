54b91175611ac8088478eb2d25c309aca070b628
Kareem Tantawi <kareem01229526319@gmail.com>
2024-12-07 16:27:26 +0200

    finish the sign in screen

--- lib/view/auth_screen/auth_screen.dart
+++ lib/view/auth_screen/auth_screen.dart
@@ -1,5 +1,6 @@
 import 'package:amazon_app_store/constants/common_function.dart';
 import 'package:amazon_app_store/utils/colors.dart';
+import 'package:country_picker/country_picker.dart';
 import 'package:flutter/material.dart';
 
 class AuthScreen extends StatefulWidget {
@@ -11,6 +12,8 @@ class AuthScreen extends StatefulWidget {
 
 class _AuthScreenState extends State<AuthScreen> {
   bool inLogin = false;
+  String currentCountryCode = '+20';
+  TextEditingController mobileController = TextEditingController();
   @override
   Widget build(BuildContext context) {
     final height = MediaQuery.of(context).size.height;
@@ -20,10 +23,10 @@ class _AuthScreenState extends State<AuthScreen> {
     return Scaffold(
       backgroundColor: white,
       appBar: PreferredSize(
-        preferredSize: const Size.fromHeight(60), // Adjust height as needed
+        preferredSize: const Size.fromHeight(60),
         child: Container(
           decoration: BoxDecoration(
-            color: white, // AppBar background color
+            color: white,
             boxShadow: [
               BoxShadow(
                 color: Colors.grey.withOpacity(0.5), // Shadow color
@@ -91,21 +94,28 @@ class _AuthScreenState extends State<AuthScreen> {
                       ),
                       child: Row(
                         children: [
-                          Container(
-                            height: height * 0.06,
-                            width: width * 0.06,
-                            decoration: BoxDecoration(
-                              shape: BoxShape.circle,
-                              border: Border.all(
-                                color: grey,
+                          InkWell(
+                            onTap: () {
+                              setState(() {
+                                inLogin = true;
+                              });
+                            },
+                            child: Container(
+                              height: height * 0.06,
+                              width: width * 0.06,
+                              decoration: BoxDecoration(
+                                shape: BoxShape.circle,
+                                border: Border.all(
+                                  color: grey,
+                                ),
+                                color: white,
                               ),
-                              color: white,
-                            ),
-                            alignment: Alignment.center,
-                            child: Icon(
-                              Icons.circle,
-                              size: height * 0.012,
-                              color: !inLogin ? transparent : secondaryColor,
+                              alignment: Alignment.center,
+                              child: Icon(
+                                Icons.circle,
+                                size: height * 0.012,
+                                color: inLogin ? secondaryColor : transparent,
+                              ),
                             ),
                           ),
                           CommonFunction.blanckSpace(0, width * 0.02),
@@ -125,6 +135,185 @@ class _AuthScreenState extends State<AuthScreen> {
                               ],
                             ),
                           )
+                        ],
+                      ),
+                    ),
+                    Container(
+                      width: width,
+                      padding: EdgeInsets.symmetric(
+                        horizontal: width * 0.03,
+                        vertical: height * 0.01,
+                      ),
+                      child: Column(
+                        children: [
+                          Row(
+                            children: [
+                              InkWell(
+                                onTap: () {
+                                  setState(() {
+                                    inLogin = false;
+                                  });
+                                },
+                                child: Container(
+                                  height: height * 0.06,
+                                  width: width * 0.06,
+                                  decoration: BoxDecoration(
+                                    shape: BoxShape.circle,
+                                    border: Border.all(
+                                      color: grey,
+                                    ),
+                                    color: white,
+                                  ),
+                                  alignment: Alignment.center,
+                                  child: Icon(
+                                    Icons.circle,
+                                    size: height * 0.012,
+                                    color:
+                                        inLogin ? transparent : secondaryColor,
+                                  ),
+                                ),
+                              ),
+                              CommonFunction.blanckSpace(0, width * 0.02),
+                              RichText(
+                                text: TextSpan(
+                                  children: [
+                                    TextSpan(
+                                      text: 'Sign in. ',
+                                      style: textTheme.bodyMedium!.copyWith(
+                                        fontWeight: FontWeight.bold,
+                                      ),
+                                    ),
+                                    TextSpan(
+                                      text: 'Already a Customer?',
+                                      style: textTheme.bodyLarge!,
+                                    ),
+                                  ],
+                                ),
+                              )
+                            ],
+                          ),
+                          CommonFunction.blanckSpace(height * 0.01, 0),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              InkWell(
+                                onTap: () {
+                                  showCountryPicker(
+                                    context: context,
+                                    onSelect: (val) {
+                                      setState(() {
+                                        currentCountryCode =
+                                            "+${val.phoneCode}";
+                                      });
+                                    },
+                                  );
+                                },
+                                child: Container(
+                                  height: height * 0.06,
+                                  width: width * 0.2,
+                                  decoration: BoxDecoration(
+                                    border: Border.all(
+                                      color: grey,
+                                    ),
+                                    color: greyShade2,
+                                    borderRadius: BorderRadius.circular(5),
+                                  ),
+                                  child: Center(
+                                    child: Text(
+                                      currentCountryCode,
+                                      style: textTheme.displaySmall!.copyWith(
+                                        fontWeight: FontWeight.w600,
+                                        fontSize: 18,
+                                      ),
+                                    ),
+                                  ),
+                                ),
+                              ),
+                              SizedBox(
+                                height: height * 0.06,
+                                width: width * 0.66,
+                                child: TextFormField(
+                                  controller: mobileController,
+                                  cursorColor: black,
+                                  // style: textTheme.displaySmall,
+                                  keyboardType: TextInputType.number,
+                                  decoration: InputDecoration(
+                                    hintText: 'Mobile number',
+                                    border: OutlineInputBorder(
+                                      borderRadius: BorderRadius.circular(5),
+                                      borderSide: BorderSide(
+                                        color: grey,
+                                      ),
+                                    ),
+                                    focusedBorder: OutlineInputBorder(
+                                      borderRadius: BorderRadius.circular(5),
+                                      borderSide: const BorderSide(
+                                        color: secondaryColor,
+                                      ),
+                                    ),
+                                    disabledBorder: OutlineInputBorder(
+                                      borderRadius: BorderRadius.circular(5),
+                                      borderSide: BorderSide(
+                                        color: grey,
+                                      ),
+                                    ),
+                                    enabledBorder: OutlineInputBorder(
+                                      borderRadius: BorderRadius.circular(5),
+                                      borderSide: BorderSide(
+                                        color: grey,
+                                      ),
+                                    ),
+                                  ),
+                                ),
+                              )
+                            ],
+                          ),
+                          CommonFunction.blanckSpace(height * 0.02, 0),
+                          ElevatedButton(
+                            onPressed: () {},
+                            style: ElevatedButton.styleFrom(
+                              minimumSize: Size(
+                                width * 0.88,
+                                height * 0.06,
+                              ),
+                              backgroundColor: amber,
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(5),
+                              ),
+                            ),
+                            child: Text(
+                              'Continue',
+                              style: textTheme.displaySmall!.copyWith(
+                                fontWeight: FontWeight.w600,
+                                fontSize: 17,
+                              ),
+                            ),
+                          ),
+                          CommonFunction.blanckSpace(height * 0.01, 0),
+                          RichText(
+                            text: TextSpan(
+                              children: [
+                                TextSpan(
+                                  text: 'By Continuing you agree to Amazon\'s ',
+                                  style: textTheme.labelMedium,
+                                ),
+                                TextSpan(
+                                  text: 'Conditions of use ',
+                                  style: textTheme.labelMedium!
+                                      .copyWith(color: blue),
+                                ),
+                                TextSpan(
+                                  text: 'and ',
+                                  style: textTheme.labelMedium,
+                                ),
+                                TextSpan(
+                                  text: 'Privacy Notice',
+                                  style: textTheme.labelMedium!
+                                      .copyWith(color: blue),
+                                )
+                              ],
+                            ),
+                          ),
                         ],
                       ),
                     ),
